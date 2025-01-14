9a47261e6729177851f70784db9b9f48389dcd5b
AlbertoRP01 <albertoriv.7@gmail.com>
2025-01-14 15:00:28 +0100

    cambios

---
+++ Herencias/src/Ej2/Furgoneta.java
@@ -0,0 +1,9 @@
+package Ej2;
+
+public class Furgoneta extends  Vehiculo{
+    private final  double
+    public Furgoneta(String matricula, TGama gama) {
+        super(matricula, gama);
+    }
+
+}
---
+++ Herencias/src/Ej2/Microbus.java
@@ -0,0 +1,7 @@
+package Ej2;
+
+public class Microbus extends  Vehiculo{
+    public Microbus(String matricula, TGama gama) {
+        super(matricula, gama);
+    }
+}
